51fdb5913d0dcc5f19d8e3afe9b087d846e6c28b
foadms <foaadmsfi@gmail.com>
2021-11-08 09:27:38 +0000

    Deploy from Azure DevOps

---
+++ component---src-pages-samples-wp-page-id-tsx-ea2ea189714e6d4bc698.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkahanbama=self.webpackChunkahanbama||[]).push([[668],{2028:function(a,n,e){e.r(n),e.d(n,{default:function(){return u}});var t=e(7294);function u(a){return t.createElement("div",null,"Props Data: ",a.data.wpPage.id)}}}]);
+//# sourceMappingURL=component---src-pages-samples-wp-page-id-tsx-ea2ea189714e6d4bc698.js.map
